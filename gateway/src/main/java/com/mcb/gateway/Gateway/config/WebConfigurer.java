54b77b9048fc48652ec17bd52dfc075e9d3a5789
Nandom Gusen <nandomgusen@yahoo.com>
2024-02-22 08:20:04 +0100

    Refactored Gateway WebConfigurer origin headers

--- gateway/src/main/java/com/mcb/gateway/Gateway/config/WebConfigurer.java
+++ gateway/src/main/java/com/mcb/gateway/Gateway/config/WebConfigurer.java
@@ -12,27 +12,18 @@ import java.util.Arrays;
 @Configuration
 public class WebConfigurer implements WebFluxConfigurer {
 
-
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedOriginPatterns(Arrays.asList("*"));
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:9090", "http://63.250.53.24"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "X-Requested-With", "X-Custom-Header"));
+        config.setAllowCredentials(true);
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        source.registerCorsConfiguration("/api/**", config);
-        source.registerCorsConfiguration("/management/**", config);
-        source.registerCorsConfiguration("/v3/api-docs", config);
-        source.registerCorsConfiguration("/swagger-ui/**", config);
-        source.registerCorsConfiguration("/*/api/**", config);
-        source.registerCorsConfiguration("/services/*/api/**", config);
-        source.registerCorsConfiguration("/*/management/**", config);
 
         return new CorsWebFilter(source);
-
     }
 }
